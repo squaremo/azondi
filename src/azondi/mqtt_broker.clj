3f8041aabdfcd76cc773f0f946e65051aa18de43
Michael Klishin <mklishin@gopivotal.com>
2014-01-23 23:40:36 +0400

    Extract MQTT method handlers

--- src/azondi/mqtt_broker.clj
+++ src/azondi/mqtt_broker.clj
@@ -21,40 +21,59 @@
 (defn authenticated? [{:keys [username password]}]
   (nil? (true? (valid-users [username password]))))
 
+;;
+;; Handlers
+;;
+
+(defn handle-connect
+  [ctx msg connections]
+  (println "Got connection!" (pr-str ctx) (pr-str msg))
+  ;; TODO Validate connection protocol name and assert version is 3.1
+  (dosync (alter connections assoc ctx (assoc msg :authenticated? (authenticated? msg))))
+  (.writeAndFlush ctx {:type :connack}))
+
+(defn handle-subscribe
+  [ctx msg subs]
+  (.writeAndFlush ctx {:type :suback})
+  (dosync
+   (alter subs (fn [subs]
+                 (reduce #(update-in %1 [%2] conj ctx)
+                         subs (map first (:topics msg)))))))
+
+(defn handle-publish
+  [ctx msg connections channel]
+  (println "Got message!" (pr-str ctx) (pr-str msg))
+  (let [conn (get @connections ctx)]
+    (when conn
+      (when channel
+        (println "putting on core.async channel")
+        (put! channel msg))
+      (println "Sending response to conn: " conn)
+      ;; Handle subscribers, although this should probably happen somewhere else now
+      (doseq [ctx (get @subs (:topic msg))]
+        (.writeAndFlush ctx msg)))))
+
+(defn handle-pingreq
+  [ctx]
+  (.writeAndFlush ctx {:type :pingresp}))
+
+(defn handle-disconnect
+  [ctx]
+  (.close ctx))
+
+;;
+;; Netty glue
+;;
+
 (defn make-channel-handler [{:keys [subs connections channel]}]
   (proxy [ChannelHandlerAdapter] []
     (channelRead [ctx msg]
       (case (:type msg)
-
-        :connect
-        (do
-          (println "Got connection!" (pr-str ctx) (pr-str msg))
-          ;; TODO Validate connection protocol name and assert version is 3.1
-          (dosync (alter connections assoc ctx (assoc msg :authenticated? (authenticated? msg))))
-          (.writeAndFlush ctx {:type :connack}))
-
-        :subscribe
-        (do (.writeAndFlush ctx {:type :suback})
-            (dosync
-             (alter subs (fn [subs]
-                           (reduce #(update-in %1 [%2] conj ctx)
-                                   subs (map first (:topics msg)))))))
-        :publish
-        (do
-          (println "Got message!" (pr-str ctx) (pr-str msg))
-          (let [conn (get @connections ctx)]
-            (when conn
-              (when channel
-                (println "putting on core.async channel")
-                (put! channel msg))
-              (println "Sending response to conn: " conn)
-              ;; Handle subscribers, although this should probably happen somewhere else now
-              (doseq [ctx (get @subs (:topic msg))]
-                (.writeAndFlush ctx msg)))))
-
-        :pingreq (.writeAndFlush ctx {:type :pingresp})
-
-        :disconnect (.close ctx)))
+        :connect    (handle-connect ctx msg connections)
+        :subscribe  (handle-subscribe ctx msg subs)
+        :publish    (handle-publish ctx msg connections channel)
+        :pingreq    (handle-pingreq ctx)
+        :disconnect (handle-disconnect ctx)))
     (exceptionCaught [ctx cause]
       (try (throw cause)
            (finally (.close ctx))))))
