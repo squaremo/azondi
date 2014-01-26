c421acc5bf2721a2745b9a4cc038ad10b1bdd0dc
Michael Klishin <mklishin@gopivotal.com>
2014-01-27 00:56:07 +0400

    Introduce a namespace for authentication functions

---
+++ src/azondi/authentication.clj
@@ -0,0 +1,22 @@
+(ns azondi.authentication
+  (:require [clojurewerkz.cassaforte.cql :refer :all]
+            [clojurewerkz.cassaforte.query :refer :all]
+            [clojurewerkz.scrypt.core :as sc]))
+
+(defn user-exists?
+  "Returns true if a user with the provided username
+   exists."
+  [^String email]
+  (not (empty? (select "users"
+                       (where :email email)
+                       (limit 1)))))
+
+(defn authenticates?
+  "Returns true if the provided user
+   successfully authenticates with the provided password."
+  [^String email ^String password]
+  (if-let [user (first (select "users"
+                               (where :email email)
+                               (limit 1)))]
+    (sc/verify password {:pword user})
+    false))
