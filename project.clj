416f80264bbf4a57bdeb966d711a7acbdca6b6c9
Michael Klishin <mklishin@gopivotal.com>
2014-01-26 19:36:05 +0400

    Bump Cassaforte

--- project.clj
+++ project.clj
@@ -18,7 +18,7 @@
 
                  ;; Back-end
                  [clojurewerkz/machine_head "1.0.0-beta5"]
-                 [clojurewerkz/cassaforte "1.3.0-beta7"]
+                 [clojurewerkz/cassaforte "1.3.0-beta8"]
                  [cheshire "5.2.0"]
 
                  ;; Jig
