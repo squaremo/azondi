7c93c53cac985292e5174f7a11f32bec2726f2c2
Michael Klishin <mklishin@gopivotal.com>
2014-01-26 21:58:09 +0400

    Add Scrypt dependency

--- project.clj
+++ project.clj
@@ -19,6 +19,7 @@
                  ;; Back-end
                  [clojurewerkz/machine_head "1.0.0-beta6"]
                  [clojurewerkz/cassaforte "1.3.0-beta8"]
+                 [clojurewerkz/scrypt     "1.1.0"]
                  [cheshire "5.3.0"]
 
                  ;; Jig
