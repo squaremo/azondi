bbe44282d5c26d0bd0099c9fa2d9fe982b607807
Michael Klishin <mklishin@gopivotal.com>
2014-02-01 16:55:05 +0400

    Turn handler state into a map

--- src/azondi/transports/mqtt.clj
+++ src/azondi/transports/mqtt.clj
@@ -15,6 +15,10 @@
 (def ^:const supported-mqtt-protocol "MQIsdp")
 (def ^:const supported-mqtt-version  3)
 
+;;
+;; CONNECT
+;;
+
 (defn supported-protocol?
   [^String protocol-name ^long protocol-version]
   (and (= protocol-name    supported-mqtt-protocol)
@@ -30,22 +34,33 @@
   [^ChannelHandlerContext ctx]
   (.close ctx))
 
+(defn valid-client-id?
+  [^String client-id]
+  ;; Section 3.1: valid client ids are between 1 and 23 characters
+  (<= 1 (.length client-id) 23))
+
+(defn accept-connection
+  [^ChannelHandlerContext ctx {:keys [username client-id has-will
+                                      clean-session]
+                               :as   msg}
+   {:keys [connections-by-ctx connections-by-client-id] :as handler-state}]
+  (let [conn {:username username
+              :client-id client-id
+              :has-will has-will
+              :will-qos (when has-will
+                          (:will-qos msg))}]
+    (dosync
+     (alter connections-by-ctx       assoc ctx conn)
+     (alter connections-by-client-id assoc client-id conn))
+    (.writeAndFlush ctx {:type :connack :return-code :accepted})
+    conn))
+
 (defn reject-connection
   [^ChannelHandlerContext ctx code]
   (doto ctx
     (.writeAndFlush {:type :connack :return-code code})
     .close))
 
-(defn accept-connection
-  [^ChannelHandlerContext ctx {:keys [username client-id]
-                               :as   msg} connections]
-  (let [conn {:username username
-              :client-id client-id
-              :will-qos (:will-qos msg)}]
-    (dosync
-     (alter connections assoc ctx conn))
-    (.writeAndFlush ctx {:type :connack :return-code :accepted})
-    conn))
 
 ;;
 ;; Handlers
@@ -57,28 +72,33 @@
                                       has-username
                                       has-password
                                       username
-                                      password] :as msg} connections]
-  ;; example msg:
-  ;; {:dup false,
-  ;;  :has-will false,
-  ;;  :has-username true,
-  ;;  :qos 0,
+                                      password
+                                      client-id] :as msg} connections]
+  ;; example connect message:
+  ;; {
+  ;;  :type :connect,
   ;;  :client-id antares.1391193099727,
-  ;;  :clean-session true,
-  ;;  :will-retain false,
+  ;;  :has-username true,
   ;;  :username michael@example.org,
   ;;  :has-password true,
+  ;;  :password michael-pwd,
+  ;;  :has-will false,
+  ;;  :will-qos 0,
+  ;;  :will-retain false,
+  ;;  :qos 0,
+  ;;  :clean-session true,
   ;;  :keepalive 60,
-  ;;  :type :connect,
   ;;  :retain false,
   ;;  :protocol-name MQIsdp,
   ;;  :protocol-version 3,
-  ;;  :password michael-pwd,
-  ;;  :will-qos 0}
+  ;;  :dup false
+  ;;  }
   (if (supported-protocol? protocol-name protocol-version)
     (if (and has-username has-password
              (auth/authenticates? username password))
-      (accept-connection ctx msg connections)
+      (if (valid-client-id? client-id)
+        (accept-connection ctx msg connections)
+        (reject-connection ctx :bad-username-or-password))
       (reject-connection ctx :bad-username-or-password))
     (do
       ;; TODO: logging
@@ -88,47 +108,54 @@
       (reject-connection ctx :unacceptable-protocol-version))))
 
 (defn handle-subscribe
-  [^ChannelHandlerContext ctx msg subs]
+  [^ChannelHandlerContext ctx msg subscriptions]
   (.writeAndFlush ctx {:type :suback})
   (dosync
-   (alter subs (fn [subs]
-                 (reduce #(update-in %1 [%2] conj ctx)
-                         subs (map first (:topics msg)))))))
+   (alter subscriptions (fn [subs]
+                          (reduce #(update-in %1 [%2] conj ctx)
+                                  subs (map first (:topics msg)))))))
 
 (defn handle-publish
-  [^ChannelHandlerContext ctx msg connections channel]
+  [^ChannelHandlerContext ctx msg {:keys [connections-by-ctx
+                                          subscriptions
+                                          channel] :as handler-state}]
   (println "Got message!" (pr-str ctx) (pr-str msg))
-  (let [conn (get @connections ctx)]
+  (let [conn (get @connections-by-ctx ctx)]
     (when conn
       (when channel
         (println "putting on core.async channel")
-        (put! channel msg))
+        #_ (put! channel msg))
       (println "Sending response to conn: " conn)
       ;; Handle subscribers, although this should probably happen somewhere else now
-      (doseq [ctx (get @subs (:topic msg))]
+      (doseq [ctx (get @subscriptions (:topic msg))]
         (.writeAndFlush ctx msg)))))
 
 (defn handle-pingreq
-  [^ChannelHandlerContext ctx]
+  [^ChannelHandlerContext ctx _]
   (.writeAndFlush ctx {:type :pingresp}))
 
 (defn handle-disconnect
-  [^ChannelHandlerContext ctx]
+  [^ChannelHandlerContext ctx _]
   (.close ctx))
 
 ;;
 ;; Netty glue
 ;;
 
-(defn make-channel-handler [{:keys [subs connections channel]}]
+(defn make-channel-handler
+  [{:keys [channel
+           connections-by-ctx
+           connections-by-client-id
+           subscriptions]
+    :as handler-state}]
   (proxy [ChannelHandlerAdapter] []
     (channelRead [^ChannelHandlerContext ctx msg]
       (case (:type msg)
-        :connect    (handle-connect ctx msg connections)
-        :subscribe  (handle-subscribe ctx msg subs)
-        :publish    (handle-publish ctx msg connections channel)
-        :pingreq    (handle-pingreq ctx)
-        :disconnect (handle-disconnect ctx)))
+        :connect    (handle-connect    ctx msg handler-state)
+        :subscribe  (handle-subscribe  ctx msg handler-state)
+        :publish    (handle-publish    ctx msg handler-state)
+        :pingreq    (handle-pingreq    ctx handler-state)
+        :disconnect (handle-disconnect ctx handler-state)))
     (exceptionCaught [^ChannelHandlerContext ctx cause]
       (try (throw cause)
            (finally (abort ctx))))))
@@ -140,7 +167,8 @@
     (let [ch (some (comp :channel system) (:jig/dependencies config))]
       (assoc-in system
                 [(:jig/id config) :jig.netty/handler-factory]
-                #(make-channel-handler {:subs (ref {})
-                                        :connections (ref {})
+                #(make-channel-handler {:subscriptions (ref {})
+                                        :connections-by-ctx (ref {})
+                                        :connections-by-client-id (ref {})
                                         :channel ch}))))
   (stop [_ system] system))
